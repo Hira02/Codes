b1d4c541e314eabad076b2e891d81dd85d09b384
Hira Saha <hirasaha02@gmail.com>
2020-05-18 18:21:33 +0530

    Create Most Frequent Subtree Sum.cpp

---
+++ Most Frequent Subtree Sum.cpp
@@ -0,0 +1,68 @@
+/*
+Problem Description:(Lc 508)
+---------------------------------------------------------------
+Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
+
+Examples 1
+Input:
+
+  5
+ /  \
+2   -3
+return [2, -3, 4], since all the values happen only once, return all of them in any order.
+Examples 2
+Input:
+
+  5
+ /  \
+2   -5
+return [2], since 2 happens twice, however -5 only occur once.
+Note: You may assume the sum of values in any subtree is in the range of 32-bit signed integer.
+
+*/
+Code:
+-------------------------------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int max_= INT_MIN;
+    int helper(TreeNode* root, map<int, int> &m, int sum){
+        if(root == NULL)
+            return 0;
+        sum+=root->val;
+        int leftSum = helper(root->left,m, 0);
+        int rightSum = helper(root->right, m, 0);
+        sum+=(leftSum+rightSum);
+        m[sum]++;
+        return sum;
+        
+    }
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        vector<int> res;
+        map<int, int> m;
+        if(root == NULL)
+            return res;
+        helper(root, m, 0);
+        for(auto c:m){
+            if(c.second>max_){
+                res.clear();
+                res.push_back(c.first);
+                max_ = c.second;
+                
+            }else if(c.second == max_){
+                res.push_back(c.first);
+            }
+        }
+        return res;
+    }
+};
