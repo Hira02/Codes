5ad77e5bdc27e3809a265f1a73788a71c8082b98
Hira Saha <hirasaha02@gmail.com>
2020-05-22 01:09:50 +0530

    Create Linked List in Binary Tree.cpp

---
+++ Linked List in Binary Tree.cpp
@@ -0,0 +1,77 @@
+/*
+Problem Description : (LC 1367)
+---------------------------------------------------
+Given a binary tree root and a linked list with head as the first node. 
+
+Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+In this context downward path means a path that starts at some node and goes downwards.
+
+ 
+
+Example 1:
+https://assets.leetcode.com/uploads/2020/02/12/sample_1_1720.png
+
+
+Input: head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Explanation: Nodes in blue form a subpath in the binary Tree.  
+Example 2:
+https://assets.leetcode.com/uploads/2020/02/12/sample_2_1720.png
+
+
+Input: head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Example 3:
+
+Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: false
+Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
+ 
+
+Constraints:
+
+1 <= node.val <= 100 for each node in the linked list and binary tree.
+The given linked list will contain between 1 and 100 nodes.
+The given binary tree will contain between 1 and 2500 nodes.
+
+
+*/
+
+Code:
+---------------------------------------------
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(root == NULL)
+            return false;
+        return (find(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right));
+        
+    }
+    bool find(ListNode* head, TreeNode* root){
+        if(head == NULL)
+            return true;
+        if(root == NULL)
+            return false;
+        if(root->val == head->val)
+            return find(head->next, root->left) || find(head->next,root->right);
+        return false;
+    }
+};
