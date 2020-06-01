a9c7a4a1b7c65e9b4f23055eba6747698cacef3e
Hira Saha <hirasaha02@gmail.com>
2020-06-01 17:22:11 +0530

    Create Invert Binary Tree.cpp

---
+++ Invert Binary Tree.cpp
@@ -0,0 +1,54 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+*/
+Code:
+---------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root){
+        if(root == NULL)
+            return ;
+        helper(root->left);
+        helper(root->right);
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        
+        helper(root);
+        return root;
+    }
+};
