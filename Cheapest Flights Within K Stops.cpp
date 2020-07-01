b40ce366b4888d64ee6b179c7e89246c7abb273a
Hira Saha <hirasaha02@gmail.com>
2020-07-01 21:19:58 +0530

    Update Cheapest Flights Within K Stops.cpp

--- Cheapest Flights Within K Stops.cpp
+++ Cheapest Flights Within K Stops.cpp
@@ -3,7 +3,8 @@ Problem Description : (LC 787)
 ----------------------------------------------------------------------------
 There are n cities connected by m flights. Each flight starts from city u and arrives at v with a price w.
 
-Now given all the cities and flights, together with starting city src and the destination dst, your task is to find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
+Now given all the cities and flights, together with starting city src and the destination dst, your task is to find the cheapest price from src to dst with up to k stops.
+If there is no such route, output -1.
 
 Example 1:
 
