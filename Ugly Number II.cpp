7bec9df94b8b98b8b291b7a02009f8b2eb91f7f2
Hira Saha <hirasaha02@gmail.com>
2020-07-04 23:43:37 +0530

    Update Ugly Number II.cpp

--- Ugly Number II.cpp
+++ Ugly Number II.cpp
@@ -18,6 +18,7 @@ n does not exceed 1690.
 */
 
 Code:
+//Approach : 1
 -------------------------------------------------------------------------------------
 class Solution {
 public:
@@ -30,25 +31,49 @@ public:
         int i2 = 0;
         int i3 = 0;
         int i5 = 0;
-        int next_ugly_2 = 2;
-        int next_ugly_3 = 3;
-        int next_ugly_5 = 5;
         int next_ugly = 1;
         for(int i = 1; i < n ; i++){//3
-             next_ugly = min(next_ugly_2,min(next_ugly_3, next_ugly_5));//4,6,5
-            cout<<next_ugly<<" ";
-            ugly[i] = next_ugly; // 4
-            if(next_ugly == next_ugly_2){ 
-                i2 = i2 + 1;
-                next_ugly_2 = ugly[i2]*2;
-            } if(next_ugly == next_ugly_3){
-                i3 = i3+1;
-                next_ugly_3 = ugly[i3]*3;
-            }if(next_ugly == next_ugly_5){
-                i5 = i5 + 1;
-                next_ugly_5 = ugly[i5]*5;
-            }
+             ugly[i] = min(ugly[i2]*2,min(ugly[i3]*3, ugly[i5]*5));
+            if(ugly[i] == ugly[i2]*2)  i2++;
+            if(ugly[i] == ugly[i3]*3)  i3++;
+            if(ugly[i] == ugly[i5]*5)  i5++;
         }
         return ugly[n-1];
+    }
+};
+
+
+------------------------------------------------------------------------------------
+    Approach : 2
+        -----------------------------------------------------------------------------------
+        //Using priority queue
+        class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if(n == 1)
+            return 1;
+        priority_queue<long, vector<long>, greater<long>> pq;
+        // pq.push(1);
+        pq.push(2);
+        pq.push(3);
+        pq.push(5);
+        long long val = 1;
+        int count= 1;
+        while(count<n){
+            val = pq.top();
+            pq.pop();
+            cout<<val<<" ";
+            if(val != pq.top()){ // to remove duplicate values
+                pq.push(val*2);
+                pq.push(val*3);
+                pq.push(val*5);
+                count++;
+            }else{
+                continue;
+            }
+        }
+        return val;
+        
+        
     }
 };
