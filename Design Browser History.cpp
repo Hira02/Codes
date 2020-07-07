5a4faf066b8576e6b0a1ea6c2760486991cf718c
Hira Saha <hirasaha02@gmail.com>
2020-07-08 01:52:55 +0530

    Update Design Browser History.cpp

--- Design Browser History.cpp
+++ Design Browser History.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Description : (LC 1472)
 --------------------------------------------------
-You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the 
+history number of steps.
 
 Implement the BrowserHistory class:
 
