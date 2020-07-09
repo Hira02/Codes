87425b734ee04202418fd021987201d1dbd270fa
Hira Saha <hirasaha02@gmail.com>
2020-07-09 14:36:27 +0530

    Create Maximum Width of Binary Tree.cpp

---
+++ Maximum Width of Binary Tree.cpp
@@ -0,0 +1,89 @@
+/*
+Problem Description : (LC 662)
+-----------------------------------------------------------------------------
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+*/
+
+Code:
+--------------------------------------------------------------------------------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unsigned getWidth(TreeNode* root, int level,unsigned nodeOrder, vector<pair<unsigned, unsigned>>& counter){
+        if(root == NULL) return 0;
+        if(counter.size() == level) counter.push_back({nodeOrder, nodeOrder});
+        else
+            counter[level].second = nodeOrder;
+        return max({counter[level].second - counter[level].first + 1, getWidth(root->left, level+1, nodeOrder*2, counter), getWidth(root->right, level+1, nodeOrder*2+1, counter)});
+    }
+    int widthOfBinaryTree(TreeNode* root) {
+        vector<pair<unsigned, unsigned>> ans;
+        return (int)getWidth(root, 0, 1, ans);
+        
+    }
+};
