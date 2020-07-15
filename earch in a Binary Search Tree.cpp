353043149b8b2f5c83aa15eb11f792b600a8719f
Hira Saha <hirasaha02@gmail.com>
2020-07-15 19:53:13 +0530

    Update earch in a Binary Search Tree.cpp

--- earch in a Binary Search Tree.cpp
+++ earch in a Binary Search Tree.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Description : (LC 700)
 -----------------------------------------------
-Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted
+with that node. If such node doesn't exist, you should return NULL.
 
 For example, 
 
