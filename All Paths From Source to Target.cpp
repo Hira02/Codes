60805295284501d6a4315a5563256d77ffdaa5f2
Hira Saha <hirasaha02@gmail.com>
2020-07-24 22:56:00 +0530

    Create All Paths From Source to Target.cpp

---
+++ All Paths From Source to Target.cpp
@@ -0,0 +1,53 @@
+/*
+Problem Description : (LC 797)
+------------------------------------------------------------------------------------------
+Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+
+The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+Example:
+Input: [[1,2], [3], [3], []] 
+Output: [[0,1,3],[0,2,3]] 
+Explanation: The graph looks like this:
+0--->1
+|    |
+v    v
+2--->3
+There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Note:
+
+The number of nodes in the graph will be in the range [2, 15].
+You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+*/
+Code:
+---------------------------------------------------------------------------------------------
+class Solution {
+public:
+    void helper( vector<vector<int>>& Map,vector<vector<int>>& ans ,  vector<int>& temp, int index, int end){
+        temp.push_back(index);
+        if(index == end){
+            ans.push_back(temp);
+        }
+        for(int i = 0 ; i < Map[index].size() ; i++){
+             int val = Map[index][i];
+            cout<<val<<"\n";
+            helper(Map, ans, temp, val, end);
+           
+            temp.pop_back();
+        }
+        
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        if(graph.size() == 0)
+            return ans;
+        unordered_map<int, vector<int>> Map;
+        int n = graph.size();
+        vector<int> temp;
+        helper(graph, ans,temp, 0, n-1);
+        return ans;
+        
+    }
+};
+//Using adjacency matrix will also work but will take O(n) space for all nodes
