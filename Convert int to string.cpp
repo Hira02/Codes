526f7c772d8ca4c496dc0fbfd0d96560247e19fa
Hira Saha <hirasaha02@gmail.com>
2020-07-31 10:47:46 +0530

    Create Convert int to string.cpp

---
+++ Convert int to string.cpp
@@ -0,0 +1,12 @@
+Code:
+--------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n = 35985;
+stringstream convert;
+convert<<n;
+string s = convert.str();
+cout<<s[2];
+    return 0;
+}
