88cd3edffec80d2421df56ed1cc3c28b1a0c2a60
Hira Saha <hirasaha02@gmail.com>
2020-08-05 02:16:42 +0530

    Update Construct Tree from Linked List.cpp

--- Construct Tree from Linked List.cpp
+++ Construct Tree from Linked List.cpp
@@ -2,19 +2,22 @@
 Problem Description : GFG
 -----------------------------------------------------------------------------------
 Given a Linked List Representation of Complete Binary Tree. The task is to construct the Binary tree.
-Note : The complete binary tree is represented as a linked list in a way where if root node is stored at position i, its left, and right children are stored at position 2*i+1, 2*i+2 respectively.
+Note : The complete binary tree is represented as a linked list in a way where if root node is stored at position i,
+its left, and right children are stored at position 2*i+1, 2*i+2 respectively.
  
 
 http://d1hyf4ir1gqw6c.cloudfront.net/wp-content/uploads/LinkedListToBST.png
 
 Input:
-First line of input contains number of testcases T. For each testcase, first line of input contains length of linked list and next line contains linked list elements.
+First line of input contains number of testcases T. For each testcase, first line of input contains length of linked list 
+and next line contains linked list elements.
 
 Output:
 Output of each test case will be the level order traversal of the the constructed binary tree.
 
 User Task:
-The task is to complete the function convert() which takes head of linked list and root of the tree as the reference. The driver code prints the level order.
+The task is to complete the function convert() which takes head of linked list and root of the tree as the reference. 
+The driver code prints the level order.
 
 Constraints:
 1 <= T <= 100
