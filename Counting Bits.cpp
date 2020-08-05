34308edabb4981bc6e8e341e0dcaf682d1ebd893
Hira Saha <hirasaha02@gmail.com>
2020-08-05 17:35:00 +0530

    Update Counting Bits.cpp

--- Counting Bits.cpp
+++ Counting Bits.cpp
@@ -3,7 +3,8 @@
 
 Problem Description  :(LC 338)
 ------------------------------------------------
-Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them 
+as an array.
 
 Example 1:
 
