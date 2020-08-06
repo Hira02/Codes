3555f54d77644386f919184e67f6e3ef30ee91c5
Hira Saha <hirasaha02@gmail.com>
2020-08-06 15:38:07 +0530

    Update Find the repeating and the missing.cpp

--- Find the repeating and the missing.cpp
+++ Find the repeating and the missing.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Description 
 --------------------------------------------------------
-Given an unsorted array of size n. Array elements are in the range from 1 to n. One number from set {1, 2, …n} is missing and one number occurs twice in the array. Find these two numbers.
+Given an unsorted array of size n. Array elements are in the range from 1 to n. One number from set {1, 2, …n} is missing and one number occurs twice in the array. 
+Find these two numbers.
 Examples:
 
  
