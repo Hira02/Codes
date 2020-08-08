af0de7717792a453838944327d63ae3044351f7d
Hira Saha <hirasaha02@gmail.com>
2020-08-08 15:27:45 +0530

    Update Longest Univalue Path.cpp

--- Longest Univalue Path.cpp
+++ Longest Univalue Path.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Description: (LC 687)
 --------------------------------------------------
-Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+Given a binary tree, find the length of the longest path where each node in the path has the same value. 
+This path may or may not pass through the root.
 
 The length of path between two nodes is represented by the number of edges between them.
 
