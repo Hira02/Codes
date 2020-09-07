1cdba1ce214bb9ea5fec0d013beec2a38e098436
Hira Saha <hirasaha02@gmail.com>
2020-09-07 14:46:24 +0530

    Update Single Number III.cpp

--- Single Number III.cpp
+++ Single Number III.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Descrption : (LC 260)
 -----------------------------------------------------------------------------------------------
-Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
+Find the two elements that appear only once.
 
 Example:
 
