7249ebeb5363a925b2e6b982e585060f0aa855f8
Hira Saha <hirasaha02@gmail.com>
2020-09-08 20:02:21 +0530

    Update Roads and Libraries.cpp

--- Roads and Libraries.cpp
+++ Roads and Libraries.cpp
@@ -4,7 +4,9 @@ Problem Description : (HackerRank)
 --------------------------------------------------------------------
 Problem Link  :https://www.hackerrank.com/challenges/torque-and-development/problem
 
-The Ruler of HackerLand believes that every citizen of the country should have access to a library. Unfortunately, HackerLand was hit by a tornado that destroyed all of its libraries and obstructed its roads! As you are the greatest programmer of HackerLand, the ruler wants your help to repair the roads and build some new libraries efficiently.
+The Ruler of HackerLand believes that every citizen of the country should have access to a library. Unfortunately, HackerLand was hit by a 
+tornado that destroyed all of its libraries and obstructed its roads! As you are the greatest programmer of HackerLand, the ruler wants your
+help to repair the roads and build some new libraries efficiently.
 
 HackerLand has  cities numbered from  to . The cities are connected by  bidirectional roads. A citizen has access to a library if:
 
@@ -16,7 +18,8 @@ image
 https://s3.amazonaws.com/hr-challenge-images/0/1481983010-b779ad2b2b-torque1.png
 The cost of repairing any road is  dollars, and the cost to build a library in any city is  dollars. If in the above example  and , we would build  roads at a cost of  and  libraries for a cost of . We don't need to rebuild one of the roads in the cycle .
 
-You are given  queries, where each query consists of a map of HackerLand and value of  and . For each query, find the minimum cost of making libraries accessible to all the citizens and print it on a new line.
+You are given  queries, where each query consists of a map of HackerLand and value of  and . For each query, find the minimum cost of
+making libraries accessible to all the citizens and print it on a new line.
 
 Function Description
 
@@ -33,7 +36,8 @@ Input Format
 The first line contains a single integer , that denotes the number of queries.
 
 The subsequent lines describe each query in the following format:
-- The first line contains four space-separated integers that describe the respective values of , ,  and , the number of cities, number of roads, cost of a library and cost of a road.
+- The first line contains four space-separated integers that describe the respective values of , ,  and , the number of cities, number of roads,
+cost of a library and cost of a road.
 - Each of the next  lines contains two space-separated integers,  and , that describe a bidirectional road that connects cities  and .
 
 Constraints
