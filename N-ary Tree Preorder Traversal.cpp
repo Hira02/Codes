dbce52f436b3573d3d6534588e5d06ca947735dd
Hira Saha <hirasaha02@gmail.com>
2020-09-22 04:01:33 +0530

    Update N-ary Tree Preorder Traversal.cpp

--- N-ary Tree Preorder Traversal.cpp
+++ N-ary Tree Preorder Traversal.cpp
@@ -12,6 +12,7 @@ Recursive solution is trivial, could you do it iteratively?
 Example 1:
 Input: root = [1,null,3,2,4,null,5,6]
 Output: [1,3,5,6,2,4]
+https://assets.leetcode.com/uploads/2018/10/12/narytreeexample.png
 
 */
 Code:
