060c3a928412f673a9e8dff7a3eb4ac676fb3b10
Hira Saha <hirasaha02@gmail.com>
2020-10-03 01:25:05 +0530

    Update Max sum in the configuration.cpp

--- Max sum in the configuration.cpp
+++ Max sum in the configuration.cpp
@@ -1,7 +1,8 @@
 /*
 Problem Description : https://practice.geeksforgeeks.org/problems/max-sum-in-the-configuration/1
 -------------------------------------------------------------------------------------------
-Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation allowed is to rotate(clock-wise or counter clock-wise) the array any number of times.
+Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation 
+allowed is to rotate(clock-wise or counter clock-wise) the array any number of times.
 
 Input:
 The first line of input is the number of test cases T. Then T test cases follow. The first line of each test case is an integer
