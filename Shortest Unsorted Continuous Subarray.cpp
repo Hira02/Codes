10c17c9cf67d14d42f253a5946c949b51910d2ab
Hira Saha <hirasaha02@gmail.com>
2021-02-26 01:49:06 +0530

    Update Shortest Unsorted Continuous Subarray.cpp

--- Shortest Unsorted Continuous Subarray.cpp
+++ Shortest Unsorted Continuous Subarray.cpp
@@ -30,6 +30,8 @@ Constraints:
 */
 //Code:
 //------------------------------------------------------------------------------------------
+
+#1:
 class Solution
 {
 public:
@@ -55,3 +57,69 @@ public:
         return end - start + 1;
     }
 };
+
+
+#2:
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int s = 0, e = n-1, i, max, min;  
+        
+  
+// step 1(a) of above algo 
+for (s = 0; s < n-1; s++) 
+{ 
+    if (arr[s] > arr[s+1]) 
+    break; 
+} 
+if (s == n-1) 
+{ 
+    // cout << "The complete array is sorted"; 
+    return 0; 
+} 
+  
+// step 1(b) of above algo 
+for(e = n - 1; e > 0; e--) 
+{ 
+    if(arr[e] < arr[e-1]) 
+    break; 
+} 
+  
+// step 2(a) of above algo 
+max = arr[s]; min = arr[s]; 
+for(i = s + 1; i <= e; i++) 
+{ 
+    if(arr[i] > max) 
+    max = arr[i]; 
+    if(arr[i] < min) 
+    min = arr[i]; 
+} 
+  
+// step 2(b) of above algo 
+for( i = 0; i < s; i++) 
+{ 
+    if(arr[i] > min) 
+    {  
+    s = i; 
+    break; 
+    }      
+}  
+  
+// step 2(c) of above algo 
+for( i = n -1; i >= e+1; i--) 
+{ 
+    if(arr[i] < max) 
+    { 
+    e = i; 
+    break; 
+    }  
+}  
+      
+// step 3 of above algo 
+
+return e-s+1; 
+    }
+};
+
+//https://www.geeksforgeeks.org/minimum-length-unsorted-subarray-sorting-which-makes-the-complete-array-sorted/
